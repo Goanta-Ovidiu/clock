704b8294fdb6f2aacb86a6f18c1f3f6e50a28921
Ovidiu Goanta <Goantza_jr@yahoo.com>
2022-02-09 21:06:05 +0100

    refactor: clock

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function updateTime() {
   const h = formatNumberString(d.getHours());
   const m = formatNumberString(d.getMinutes());
   const s = formatNumberString(d.getSeconds());
-  const bodyBackground = document.querySelector("body");
 
   const sec = d.getSeconds();
   const min = d.getMinutes();
@@ -21,8 +20,8 @@ function updateTime() {
   secondsEl.style.setProperty("--seconds", sec * 6 + "deg");
   minutesEl.style.setProperty("--minutes", min * 6 + "deg");
   hoursEl.style.setProperty("--hours", hours * 30 + "deg");
-  sunChangeMoon(sun, sunny);
-  sunChangeSun(moon, moond);
+  sunChangeMoon(moon, moond);
+  sunChangeSun(sun, sunny);
 }
 
 function formatNumberString(n) {
@@ -31,17 +30,28 @@ function formatNumberString(n) {
 }
 
 function sunChangeMoon(e, x) {
+  const stars = document.querySelector(".stars");
   if (e >= 18 || e <= 7) {
-    x.style.display = "none";
-  } else {
     x.style.display = "block";
+    stars.style.display = "visible";
+  } else {
+    stars.style.display = "none";
+    x.style.display = "none";
   }
 }
 function sunChangeSun(e, x) {
+  const bodyBackground = document.querySelector("body");
+  const cloud = document.querySelector(".cloud");
+  const cloudDown = document.querySelector(".cloud_down");
   if (e <= 18 && e >= 7) {
-    x.style.display = "none";
-  } else {
     x.style.display = "block";
+    bodyBackground.style.backgroundColor = "blue";
+    cloud.style.display = "visible";
+    cloudDown.style.display = "visible";
+  } else {
+    x.style.display = "none";
+    cloud.style.display = "none";
+    cloudDown.style.display = "none";
   }
 }
 updateTime();
